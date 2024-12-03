e7f64f3c2064ece1d9772c8af8d0e900607ddc13
coredev <sayyidsilam@Sayyids-MacBook-Air.local>
2024-12-04 00:30:29 +0700

    create table and one to many relationship on the  order table

---
+++ app/Models/Order.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    /** @use HasFactory<\Database\Factories\OrderFactory> */
+    use HasFactory;
+    protected $fillable = ["payment_method", "address", "user_id"];
+}
